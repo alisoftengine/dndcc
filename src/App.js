0ee62ededd672d524ddfff2c705bd1196ea34e81
KWard <pcward.k@gmail.com>
2021-02-04 09:24:04 -0700

    rendering data from api call, nav bar working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Header from './Components/Header';
 import { Route } from 'react-router-dom';
+import Characters from './Components/Characters';
+import CharacterInfo from './Components/CharacterInfo';
 
 function App() {
    return (
       <div className='App'>
          <Header />
          <main>
-            <Route path='/'>To Home</Route>
-            <Route path='/characters'>To Characters</Route>
+            <Route exact path='/' />
+            <Route exact path='/characters' render={() => <Characters />} />
+            <Route
+               exact
+               path='/characters/:id'
+               render={routerProps => (
+                  <CharacterInfo match={routerProps.match} />
+               )}
+            />
          </main>
       </div>
    );
 }
-
+// <Route exact path='/characters/:id' render={(routerProps) => <CharacterDetails match={routerProps.match}/>}/>
 export default App;
