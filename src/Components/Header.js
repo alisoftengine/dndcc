ba4516740dd9a0aa820507eee487d75b1dadd43c
KWard <pcward.k@gmail.com>
2021-02-05 13:30:29 -0700

    bootstrap styling and Styles.css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,18 @@
-// import Nav from './Nav';
 import Navigation from './Navigation';
+import { Jumbotron } from 'react-bootstrap';
+import '../Styles/Header.css';
 
 export default function Header() {
    return (
-      <div>
-         <h1>Dungeons & Dragons Character Creator</h1>
-
-         <Navigation />
-      </div>
+      <Jumbotron>
+         <div className='container-fluid'>
+            <div className='header'>
+               <h1>Dungeons & Dragons Character Creator</h1>
+            </div>
+            <Navigation />
+         </div>
+      </Jumbotron>
    );
 }
+
+//need color scheme and font foe header
