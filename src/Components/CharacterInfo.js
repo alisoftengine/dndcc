2693eead458664e028d8bfa26df0222e8d5fcedd
fahali <fahim.ali.204@gmail.com>
2021-02-07 11:25:31 -0500

    Added styles for CharacterInfo

--- src/Components/CharacterInfo.js
+++ src/Components/CharacterInfo.js
@@ -1,95 +1,99 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Card, Table } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
+
+import '../Styles/CharacterInfo.css';
 
 export default function CharacterInfo({ match }) {
    const [character, setCharacter] = useState(null);
+
    const fetchCharacter = async id => {
-      await fetch(`https://dndcc-api.herokuapp.com/characters/${id}`)
-         .then(res => res.json())
-         .then(res => {
-            setCharacter(res);
-         })
-         .catch(console.error);
+      try {
+         const response = await axios(
+            `https://dndcc-api.herokuapp.com/characters/${id}`
+         );
+         return response.data;
+      } catch (error) {
+         console.log(error);
+      }
    };
+
    useEffect(() => {
-      fetchCharacter(match.params.id);
+      fetchCharacter(match.params.id).then(data => setCharacter(data));
    }, [match.params.id]);
+
+   const friendlyDate = dateString =>
+      new Date(dateString).toLocaleDateString('en-US', {
+         year: 'numeric',
+         month: 'long',
+         day: 'numeric'
+      });
+
    return (
       character && (
-         <div>
-            <Card>
-               <Card.Body>
-                  <Card.Header>
-                     <Card.Title>
-                        <div className='inlineDisplay'>
-                           {character.player ? (
-                              <h3>Player: {character.player} </h3>
-                           ) : null}
-                           {character.campaign ? (
-                              <h3>Campaign: {character.campaign} </h3>
-                           ) : null}
-                        </div>
-                     </Card.Title>
-                  </Card.Header>
-                  <Card.Text>
-                     <h4>
-                        Hero: {character.name} ({character.sex})
-                     </h4>
-                     <p>
-                        A {character.background} {character.race}{' '}
-                        {character.class} ({character.alignment}).
-                     </p>
-                  </Card.Text>
-                  <div className='attributes'>
-                     <Card.Subtitle className='attribute-title'>
-                        <h4>Attributes</h4>
-                     </Card.Subtitle>
-                     <Table
-                        striped
-                        bordered
-                        hover
-                        size='sm'
-                        className='attribute-table'>
-                        <tbody>
-                           <tr>
-                              <td>Strength</td>
-                              <td>{character.abilities.strength}</td>
-                           </tr>
-                           <tr>
-                              <td>Dexteriy</td>
-                              <td>{character.abilities.dexterity}</td>
-                           </tr>
-                           <tr>
-                              <td>Constitution</td>
-                              <td>{character.abilities.constitution}</td>
-                           </tr>
-                           <tr>
-                              <td>Intelligence</td>
-                              <td>{character.abilities.intelligence}</td>
-                           </tr>
-                           <tr>
-                              <td>Wisdom</td>
-                              <td>{character.abilities.wisdom}</td>
-                           </tr>
-                           <tr>
-                              <td>Charisma</td>
-                              <td>{character.abilities.charisma}</td>
-                           </tr>
-                        </tbody>
-                     </Table>
-                  </div>
-                  <Card.Footer as='h6'>
-                     <p>Date Created: {character.created}</p>
-                     <Link
-                        to={`/characters/edit/${character._id}`}
-                        key={character._id}>
-                        Edit {character.name}
-                     </Link>
-                  </Card.Footer>
-               </Card.Body>
-            </Card>
-         </div>
+         <Card className='character-card'>
+            <Card.Header>
+               <p className='character-name'>{character.name}</p>
+            </Card.Header>
+            <Card.Body>
+               <Card.Title>
+                  A {character.sex} {character.race} {character.class} of{' '}
+                  {character.background} origins ({character.alignment}).
+               </Card.Title>
+               <Card.Subtitle className='optional-info'>
+                  {character.player && <p>Creator: {character.player}</p>}
+                  {character.campaign && <p>Campaign: {character.campaign}</p>}
+               </Card.Subtitle>
+               <Table
+                  striped
+                  hover
+                  size='sm'
+                  variant='dark'
+                  className='attribute-table'>
+                  <thead>
+                     <tr className='ability-header'>
+                        <th>Ability</th>
+                        <th>Score</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                        <td>Strength</td>
+                        <td>{character.abilities.strength}</td>
+                     </tr>
+                     <tr>
+                        <td>Dexterity</td>
+                        <td>{character.abilities.dexterity}</td>
+                     </tr>
+                     <tr>
+                        <td>Constitution</td>
+                        <td>{character.abilities.constitution}</td>
+                     </tr>
+                     <tr>
+                        <td>Intelligence</td>
+                        <td>{character.abilities.intelligence}</td>
+                     </tr>
+                     <tr>
+                        <td>Wisdom</td>
+                        <td>{character.abilities.wisdom}</td>
+                     </tr>
+                     <tr>
+                        <td>Charisma</td>
+                        <td>{character.abilities.charisma}</td>
+                     </tr>
+                  </tbody>
+               </Table>
+            </Card.Body>
+            <Card.Footer>
+               <p>Created on {friendlyDate(character.created)}</p>
+               <div className='controls'>
+                  <Button variant='dark'>EDIT</Button>
+                  <Button variant='danger'>DELETE</Button>
+               </div>
+            </Card.Footer>
+         </Card>
       )
    );
 }
